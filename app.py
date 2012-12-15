1e68cf4464323eb78d6bef582e20783fe2c02f08
Gleicon Moraes <gleicon@gmail.com>
2012-12-14 23:19:25 -0200

    host fix

--- app.py
+++ app.py
@@ -7,5 +7,5 @@ import bottle
 def index():
     return bottle.static_file('park-recs-pyramid_1500.jpg', 'public/')
 
-bottle.run(server='gevent', port=os.environ.get('PORT', 5000))
+bottle.run(server='gevent', port=os.environ.get('PORT', 5000), host="0.0.0.0")
 
