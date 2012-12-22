a963103133b4e0481e4ad220a444ec06b63f494b
Gleicon Moraes <gleicon@gmail.com>
2012-12-22 18:49:39 -0200

    js fb api

--- app.py
+++ app.py
@@ -7,6 +7,15 @@ import jobs
 
 q = None
 
+def get_fb_user_graph(request):
+    user = facebook.get_user_from_cookie(request.cookies, os.environ.get('FB_APP_ID'), os.environ.get('FB_APP_SECRET')) 
+    if not user: return None
+
+    graph = facebook.GraphAPI(user['access_token']).get_object("me")
+    graph['uid'] = user['uid']
+
+    return graph    
+
 @bottle.get('/api/check')
 def index():
     return "ok"
@@ -15,13 +24,21 @@ def index():
 def index():
     return jobs.random_quote(q)
 
+@bottle.get('/fb')
+def febe():
+    return bottle.template('public/fb-canvas.html', fb_app_id=os.environ.get('FB_APP_ID'))
+
 @bottle.get('/')
 def index():
     return bottle.static_file('index.html', 'public/')
 
 @bottle.post('/fb')
 def fb():
-    return bottle.static_file('fb-canvas.html', 'public/')
+    return bottle.template('public/fb-canvas.html', fb_app_id=os.environ.get('FB_APP_ID'))
+
+@bottle.post('/api/fb/timeline')
+def fb_post():
+    pass
 
 @bottle.get('/<filepath:path>')
 def server_static(filepath):
